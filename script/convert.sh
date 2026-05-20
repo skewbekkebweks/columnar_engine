66416a098111d29f137dd3348d02e3eb723d9bc5
Mikhail Glazov <mihail.glazov2015@yandex.ru>
2026-05-20 15:24:37 +0300

    fix

--- script/convert.sh
+++ script/convert.sh
@@ -3,4 +3,4 @@ set -e
 INPUT_CSV="$1"
 COLUMNAR="$2"
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-"$ROOT/build/bin/csv_to_columnar" --input "$INPUT_CSV" --output "$COLUMNAR" --schema "$ROOT/script/hits_schema.csv" --delimiter $'\t'
+"$ROOT/build/bin/csv_to_columnar" --input "$INPUT_CSV" --output "$COLUMNAR" --schema "$ROOT/script/hits_schema.csv"
